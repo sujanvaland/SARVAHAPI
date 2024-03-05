023dc6124ee77f7c93c7fc1ed8863a77a3c5fcd5
KAMAL <KAMAL@KAMAL-PC>
2024-03-05 14:58:08 +0530

    sa

--- SpiritualNetwork.API/Services/Interface/IJobService.cs
+++ SpiritualNetwork.API/Services/Interface/IJobService.cs
@@ -11,9 +11,10 @@ namespace SpiritualNetwork.API.Services.Interface
         public Task<JobPost> SaveUpdateJobPost(JobPostReq req, int userId);
         public Task<JsonResponse> DeleteJobPost(int Id);
         public Task<Application> SaveApplication(int JobId, int userId);
-        public Task<JsonResponse> GetAllJobs(getJobReq req);
+        public Task<JsonResponse> GetAllJobs(getJobReq req, int size, int UserId);
         public Task<JsonResponse> ToggleBookmark(int postid, int userid);
         public Task<JsonResponse> GetAllBookmarkJobs(int userId);
+        public Task<JsonResponse> GetJobById(int JobId);
 
 
     }
